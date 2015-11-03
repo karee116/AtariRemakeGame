4487af65b5760af1929cf215f6a081735b66d33b
Krystal Grant <kgrant@utc4me.org>
2015-11-03 08:36:40 -0500

    Initial Commit
    Once again, new commit for this game, still a lot of work to do but will
    get to it ASAP.

---
+++ Atari Remake/AtariRemake/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	[SerializeField] Transform player;
+
+	Vector3 offset;
+
+	void Start ()
+    {
+		offset = transform.position - player.position;
+	}
+
+	void FixedUpdate ()
+	{
+
+
+}
---
+++ Atari Remake/AtariRemake/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField] float Speed = 5.0f;
+
+	Vector3 m_Movement;
+	Rigidbody2D m_PlayerRigidbody;
+	
+	void Awake ()
+	{
+		m_PlayerRigidbody = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update ()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		Move (h);
+	}
+
+	void Move(float h)
+	{
+		m_Movement.Set (h, 0, 0);
+		m_Movement = m_Movement.normalized * Speed * Time.deltaTime;
+		m_PlayerRigidbody.MovePosition (transform.position + m_Movement);
+	}
+}
