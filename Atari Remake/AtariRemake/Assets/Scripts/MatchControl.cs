277a87bc91b27e0db643ecf8a430d1bdfcb7df1b
Krystal Grant <kgrant@utc4me.org>
2015-12-11 09:26:29 -0500

    Commit #12
    Made animation to encompass the appearance and disappearance of objects
    from the light. Almost done, nee to make spawn system, points, lives,
    add sprites, add doors and controls for other objects.

--- Atari Remake/AtariRemake/Assets/Scripts/MatchControl.cs
+++ Atari Remake/AtariRemake/Assets/Scripts/MatchControl.cs
@@ -4,28 +4,44 @@ using System.Collections;
 public class MatchControl : MonoBehaviour {
 	
 	// public bool isLit;
-	Animator m_anim;
+	// CircleCollider2D m_range;
+	Color obj;
+	Animator otherAnim;
 
 	void Awake()
 	{
-		m_anim = GetComponent<Animator> ();
+		// m_range = GetComponent<CircleCollider2D>();
 		// isLit = false;
 	}
 
 	void Update ()
 	{
-		if ((Input.GetKeyDown (KeyCode.LeftControl) || Input.GetKeyDown (KeyCode.RightControl)) && m_anim.GetBool("Light") != true) {
+		if (Input.GetKeyDown (KeyCode.LeftControl) || Input.GetKeyDown (KeyCode.RightControl)) {
 			StartCoroutine(Timer (5));
 			ScoreControl.use += 1;
 			print (ScoreControl.use);
 		}
 	}
 	
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Item")
+		{
+			otherAnim = other.gameObject.GetComponent<Animator>();
+			otherAnim.SetBool("Appear", true);
+		}
+	}
+	
+	void OnTriggerExit2D (Collider2D other)
+	{
+		obj = other.gameObject.GetComponent<SpriteRenderer>().color;
+		obj.a -= .02f;
+		print ("bye");
+	}
+	
 	IEnumerator Timer (int time)
 	{
-		m_anim.SetBool("Light", true);
 		yield return new WaitForSeconds (time);
-		m_anim.SetBool("Light", false);
 	}
 
 }
